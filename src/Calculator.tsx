4dd5ef248517bb9702eef3fb049a4d4e9cfc3383
unknown <solomonpipi1@gmail.com>
2024-06-26 13:02:51 +0100

    imported operator types and started handle symbol function

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,12 +1,12 @@
 import { useMemo, useState } from "react";
 import Buttons from "./components/Buttons";
 import Input from "./components/Input";
+import { Operators } from "./types/operations";
 
 const Calculator = () => {
   const [input, setInput] = useState<string | null>(null);
   const [total, setTotal] = useState<number>(0);
   const [hasError, setHasError] = useState<boolean>(false);
-
   const [oldInput, setOldInput] = useState<string | null>(null);
   const [showOldInput, setShowOldInput] = useState<boolean>(false);
   const [prevSymbol, setPrevSymbol] = useState<string | null>(null);
@@ -16,6 +16,23 @@ const Calculator = () => {
     () => (showOldInput ? oldInput : input),
     [input, showOldInput]
   );
+
+  const operations: Operators = {
+    "+": (n1, n2) => n1 + n2,
+    "-": (n1, n2) => n1 - n2,
+    X: (n1, n2) => n1 * n2,
+    "÷": (n1, n2) => n1 / n2,
+    "%": (_n1, n2) => n2 / 100,
+    "+/-": (_n1, n2) => n2 * -1,
+  };
+
+  //   const calculate = (
+  //     buffer: number,
+  //     currentTotal: number,
+  //     symbol: string
+  //   ): number => {
+  //     return operations[symbol](currentTotal, buffer);
+  //   };
 
   const handleButtonPress = (value: string): void => {
     if (showOldInput) {
@@ -39,7 +56,31 @@ const Calculator = () => {
     }
   };
 
-  const handleSymbol = (symbol: string) => {};
+  const handleSymbol = (symbol: string) => {
+    switch (symbol) {
+      case "C":
+        // clear function
+        break;
+      case "=":
+        // when equal is pressed
+        break;
+      case "%":
+      case "+/-":
+        //handle logic for performing negative/positive or percentage conversion
+        break;
+      case ".":
+        // handle decimal
+        break;
+      case "+":
+      case "-":
+      case "✕":
+      case "÷":
+        // standard operations
+        break;
+      default:
+        break;
+    }
+  };
 
   const storeNumtoScreen = (num: string) => {
     setInput((prev) =>
--- src/types/operations.ts
+++ src/types/operations.ts
@@ -1,7 +1,7 @@
 export type Operators = {
     "+":(n1: number, n2:number) => number;
     "-":(n1: number, n2:number) => number;
-    "x":(n1: number, n2:number) => number;
+    "X":(n1: number, n2:number) => number;
     "÷":(n1: number, n2:number) => number;
     "%":(n1: number, n2:number) => number;
     "+/-":(n1: number, n2:number) => number;
